efe31c351fd38fff67bcde3700756cb732eac913
infwba291 <Thomas.Haag@haw-hamburg.de>
2025-10-29 17:30:48 +0100

    erste Implementierung von errorHandler

--- Programs/Aufgabe1/Src/errors.c
+++ Programs/Aufgabe1/Src/errors.c
@@ -1,5 +1,34 @@
 #include "errors.h"
+#include "display.h"
+#include <stdlib.h>
+
+#define LEN_ERR_MSG 256
 
 void error_handler(int errorCode){
+    char *errorMsg;
+    setErrMode();
+    switch (errorCode) {
+        case STACK_OVERFLOW:
+        break;
+        case STACK_UNDERFLOW:
+        break;
+        case NUMBER_OVERFLOW:
+        break;
+        case NUMBER_UNDERFLOW:
+        break;
+        case DIVISION_WITH_ZERO:
+            break;
+        case UNEXPECTED_INPUT:
+            break;
+        case STACK_INITILASITION_FAILED:
+            break;
+        case STACK_NOT_INITALISED:
+            errorMsg = "Stack wurde nicht initialisiert. Das Programm wird beendet";
+            printStdout(errorMsg);
+            exit(errorCode);
+            break;
+        default:
+            errorMsg = "Unbekannter errorCode. Das Programm wird beendet";
+    }
     return;
 }
