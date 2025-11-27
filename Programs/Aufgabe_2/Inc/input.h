98ee62f1b29eb1b7aff798c22b6148aeaf44215e
nayeliend11 <wkn896@haw-hamburg.de>
2025-11-27 13:22:47 +0100

    input

--- Programs/Aufgabe_2/Inc/input.h
+++ Programs/Aufgabe_2/Inc/input.h
@@ -1,11 +1,20 @@
 #ifndef INPUT_H
 #define INPUT_H
+#include "stdbool.h"
 
 typedef enum {
-
+    // Kanal A low, Kanal B low 
     PHASE_A = 0x00,
-    PHASE_B = 0x01,
-    PHASE_C = 0x02,
-    PHASE_D = 0x03
+    // Kanal A high, Kanal B low 
+    PHASE_B = 0x02,
+    // Kanal A high, Kanal B high 
+    PHASE_C = 0x03,
+    // Kanal A low, Kanal B high 
+    PHASE_D = 0x01
 }phase_t;
 #endif
+
+bool input_A();
+bool input_B();
+
+void pins_aktualisieren();
